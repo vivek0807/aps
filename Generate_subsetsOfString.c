3dd80464545b180e40e6c14ae950df3b456c3e2e
vivek0807 <35535866+vivek0807@users.noreply.github.com>
2019-02-09 10:36:52 +0530

    Create Generate_subsetsOfString.c

---
+++ Generate_subsetsOfString.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <String.h>
+#include <math.h>
+int main() {
+
+    char data[]={'a','b','c'};
+    int size=3;
+
+    int data_size=pow(2,size);
+
+    for(int i=0;i<data_size;i++){
+
+        for(int j=0;j<size;j++){
+
+            if(i &(1<<j)){
+                //printf("Andopr--%d",(i &(1<<j)));
+                printf("%c",data[j]);
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+} 
