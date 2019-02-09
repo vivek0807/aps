bae40bf785cbcc77648f86e75020d2952ec8eea2
vivek0807 <35535866+vivek0807@users.noreply.github.com>
2019-02-09 10:30:42 +0530

    Generate the maximum subset

--- main.c
+++ main.c
@@ -1,23 +1,47 @@
 #include <stdio.h>
-#include <String.h>
-#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
 int main() {
 
-    char data[]={'a','b','c'};
-    int size=3;
+    int TheNumber;
+    scanf("%d",&TheNumber);
+    int no;
+    printf("Enter number of multiples\n");
+    scanf("%d",&no);
+    int multiples[no];
+    bool found=false;
 
-    int data_size=pow(2,size);
-
-    for(int i=0;i<data_size;i++){
-
-        for(int j=0;j<size;j++){
-
-            if(i &(1<<j)){
-                //printf("Andopr--%d",(i &(1<<j)));
-                printf("%c",data[j]);
-            }
-        }
-        printf("\n");
+    for (int i = 0; i <no ; ++i) {
+        printf("\nEnter\n");
+        scanf("%d",&multiples[i]);
     }
-    return 0;
+    int arr[TheNumber+1];
+    for (int i = 0; i <(TheNumber+1) ; ++i) {
+            arr[i]=0;
+    }
+    int windowsize;
+    int index=0;
+    for (int j = 0; j < 3 ; j++) {
+        windowsize=multiples[j];
+            index=windowsize;
+            printf("Window Size %d\n",windowsize);
+        for (int i = 0; i < (TheNumber+1); ++i) {
+            if((i%windowsize==0)){
+                ++arr[i];
+               // printf("Incrementi to 1 At %d %d\n",i,arr[i]);
+                found=true;
+            }
+            else if (arr[i]==1 && found==false &&((i+windowsize)<=(TheNumber))){
+                arr[windowsize+i]++;
+                printf("New value at is %d at %d\n",arr[i+windowsize], i+windowsize);
+            }
+            found=false;
+        }
+
+
+    }
+
+
 }
